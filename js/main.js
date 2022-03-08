5cb7b9b440702663a16de914e574dfd51ffe10f7
tanjinajui <tanjinaakter994@gmail.com>
2022-03-08 23:26:17 +0600

    index-js-css-file-add

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+$('.responsive').slick({
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      
+    ]
+  });
